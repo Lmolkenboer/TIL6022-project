89b21c83886a50699e7c48465441493b4bd57f62
Pimfromthefields <105209008+Pimfromthefields@users.noreply.github.com>
2022-10-12 10:48:27 +0200

    Update Plots pim.py

--- Plots pim.py
+++ Plots pim.py
@@ -5,3 +5,10 @@ Created on Wed Oct 12 09:56:40 2022
 @author: pim
 """
 
+#Percentage of male vs female shopping per category
+df = px.data.tips()
+fig = px.histogram(
+    df, x="total_bill", y="tip", color="sex", 
+    hover_data=df.columns
+)
+fig.show()
